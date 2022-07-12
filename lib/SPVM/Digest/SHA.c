a38770d1b93215a660f1f6040f74afb6e9058576
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-07-13 08:28:20 +0900

    Add SPVM native files

---
+++ lib/SPVM/Digest/SHA.c
@@ -0,0 +1,12 @@
+#include "spvm_native.h"
+
+
+
+int32_t SPVM__Digest__SHA__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+
---
+++ lib/SPVM/Digest/SHA.pm
@@ -0,0 +1,45 @@
+package SPVM::Digest::SHA;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::Digest::SHA - Digest::SHA is a SPVM module
+
+=head1 Usage
+
+  use Digest::SHA;
+  
+=head1 Description
+
+C<Digest::SHA> is a L<SPVM> module.
+
+=head1 Field Methods
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 [--user-name], all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
